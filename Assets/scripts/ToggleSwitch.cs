a35e776826c3c7b4ed71a4ce4842c2c0a2191d1b
esraakhattat <42725783+esraakhattat@users.noreply.github.com>
2020-07-18 22:43:48 +0200

    SwitchToggle

--- Assets/scripts/ToggleSwitch.cs
+++ Assets/scripts/ToggleSwitch.cs
@@ -1,17 +1,72 @@
-﻿using UnityEngine;
+﻿
+using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.EventSystems;
-public class ToggleSwitch : MonoBehaviour
+using DG.Tweening;
+
+public class ToggleSwitch : MonoBehaviour , IPointerDownHandler
 {
+    [SerializeField]
+    private bool _isOn = false;
+    public bool isOn
+    {
+        get
+        {
+            return _isOn;
+        }
+    }
+    [SerializeField]
+    private RectTransForm toggleIndicator;
+    [SerializeField]
+    private Image backgroundImage;
+    [SerializeField]
+    private Color onColor;
+    [SerializeField]
+    private Color offColor;
+    private float offX;
+    private float OnX;
+    [SerializeField]
+    private float tweenTime = 0.25f;
+
+    public delegate void ValueChanged(bool value);
+    public event ValueChanged valueChanged;
     // Start is called before the first frame update
     void Start()
     {
-        
+        offX = toggleIndicator.anchoredPosition.x;
+        OnX = backgroundImage.rectTransform.rect.width - toggleIndicator.rect.width;
     }
-
-    // Update is called once per frame
-    void Update()
+    private void OnEnable()
     {
-        
+        Toggle(isOn);
+    }
+    private void Toggle (bool value)
+    {
+        if (value != isOn)
+        {
+            _isOn = value;
+            ToggleColor(isOn);
+            MoveIndicator(isOn);
+            if (valueChanged != null)
+                valueChanged(isOn);
+        }
+    }
+    private void ToggleColor(bool value)
+    {
+        if (value)
+            backgroundImage.DOColor(onColor, tweenTime);
+        else
+            backgroundImage.DOColor(offColor, tweenTime);
+    }
+    private void MoveIndicator(bool value)
+    {
+        if (value)
+            toggleIndicator.DOAnchorPosX(OnX, tweenTime);
+        else
+            toggleIndicator.DOAnchorPosX(offX, tweenTime);
+    }
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        Toggle(!isOn);
     }
 }
