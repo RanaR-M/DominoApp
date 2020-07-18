81fe471630410fa4e1e2a15f4490ca7a2295a1de
RanaR-M <59890817+RanaR-M@users.noreply.github.com>
2020-07-18 18:33:16 +0200

    pause menu

---
+++ Assets/scripts/pauseControl.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pauseControl : MonoBehaviour
+{
+   public GameObject pauseMenu;
+
+   public void PauseGame(){
+       pauseMenu.SetActive(true);
+   }
+
+   public void ResumeGame(){
+       pauseMenu.SetActive(false);
+   }
+}
