db12421b1b6246eec4d438a9ed96cdc67fc796af
RanaR-M <59890817+RanaR-M@users.noreply.github.com>
2020-08-08 00:20:28 +0200

    comments

--- Assets/scripts/pauseControl.cs
+++ Assets/scripts/pauseControl.cs
@@ -2,13 +2,20 @@
 
 public class pauseControl : MonoBehaviour
 {
-   public GameObject pauseMenu;
+    /* to use this code you need two objects: the control object that you attach the script too and the menu you want to control 
+     * the menu should be an empty object that has the background only and it's children are the components of the menu eg: buttons
+     * you should have at least two buttons to use these functions
+     * the PauseGame function activates the menu you chose, while the ResumeGame function deactivates it.
+     * for example you should have a pause button in the regual screen that it's onclick activates the PauseGame function
+     * and a exit button that it's onclick function uses the ResumeGame in the menu that would go back to the normal screen.
+     */
+    public GameObject pauseMenu;
 
-   public void PauseGame(){
-       pauseMenu.SetActive(true);
-   }
+    public void PauseGame() {
+        pauseMenu.SetActive(true);
+    }
 
-   public void ResumeGame(){
-       pauseMenu.SetActive(false);
-   }
+    public void ResumeGame() {
+        pauseMenu.SetActive(false);
+    }
 }
