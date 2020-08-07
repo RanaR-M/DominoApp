9adec5048dc76d2a1d544b90865a6e98cc4a5d6a
RanaR-M <59890817+RanaR-M@users.noreply.github.com>
2020-08-08 00:31:25 +0200

    comments  2

--- Assets/scripts/menuControl.cs
+++ Assets/scripts/menuControl.cs
@@ -2,6 +2,15 @@
 using UnityEngine.SceneManagement;
 
 public class menuControl : MonoBehaviour {
+
+    /* to use this code you need two Canvases one is the normal screen the other is menu you want to display
+     * add to these canvases the CanvasGroup component then make an object and attach this script to it 
+     * in the script put the canvases as the names suggest
+     * you need two buttons to use this script one to access the menu and the other to close it
+     * to the button that access the menu attach the QuitMenuDisplay method and to the button the closes it use the normalDisplay
+     * the update method is used to access the quitMenu with the press of the return button in android and the ESC button on pc
+     * 
+     */
     public CanvasGroup quitMenu;
     public CanvasGroup mainPage;
     //navigation navigationObject;
